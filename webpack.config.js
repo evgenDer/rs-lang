3f2d2beac7d800a9e3a52f045d7393645c4252f3
ALEX\alex <lyssenko.alex@gmail.com>
2020-06-14 00:37:19 +0600

    init: project structure

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+// eslint-disable-next-line no-unused-vars
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+    watch: !isProduction,
+    entry: ['./src/js/index.js', './src/sass/style.scss'],
+    output:
+    {
+      path: path.join(__dirname, '/dist'),
+      filename: 'script.js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /.(mp3)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: 'audio',
+              name: '[name].[ext]',
+            },
+          }],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        }, {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+          ],
+        }, {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        }, {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader',
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/pages/main.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: './src/pages/dictionary.html',
+        inject: 'body',
+        filename: 'dictionary.html',
+      }),
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: './src/pages/games.html',
+        inject: 'body',
+        filename: 'games.html',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: './src/img',
+            to: 'assets/img',
+          },
+          {
+            from: './src/audio',
+            to: 'assets/audio',
+          },
+        ],
+      }),
+    ],
+  };
+  return config;
+};
