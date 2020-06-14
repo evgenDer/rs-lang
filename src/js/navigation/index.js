cad2052e23ef3a8967495fbd8a5e5d2b75a6a770
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-06-14 19:52:04 +0300

    feat: implement menu closing on the outside click

--- src/js/navigation/index.js
+++ src/js/navigation/index.js
@@ -3,10 +3,22 @@ const navBtn = document.querySelector('.hamburger__line');
 const hamburger = document.querySelector('.hamburger');
 
 
+function closeNavigationMenu() {
+  nav.classList.add('navigation_hidden');
+  navBtn.classList.remove('hamburger__line_active');
+}
+
+
 function addNavigationClickHandler() {
   hamburger.addEventListener('click', () => {
     nav.classList.toggle('navigation_hidden');
     navBtn.classList.toggle('hamburger__line_active');
+  });
+
+  document.addEventListener('click', ({ target }) => {
+    if (!nav.contains(target) && (!hamburger.contains(target))) {
+      closeNavigationMenu();
+    }
   });
 }
 
