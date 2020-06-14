f0183506ee0c3efab536db36ba16abcfe3a7afae
Evgeniya <evgeniya375447249020@gmail.com>
2020-06-15 00:02:22 +0300

    fix: identificate user

---
+++ src/js/utils/checks.js
@@ -0,0 +1,15 @@
+import { LIFETIME_TOKEN } from './constants';
+
+export { LIFETIME_TOKEN } from './constants';
+
+function isValidToken() {
+  const currentDateString = new Date().toString();
+  const tokenDateString = localStorage.getItem('date');
+  return (Date.parse(currentDateString) - Date.parse(tokenDateString) <= LIFETIME_TOKEN);
+}
+
+function isNewUser() {
+  return !(isValidToken() && localStorage.getItem('userId') !== null);
+}
+
+export { isValidToken, isNewUser };
