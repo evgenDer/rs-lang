d8eab1b0d55060c74a577a691d039aac5bec6bda
Evgeniya <evgeniya375447249020@gmail.com>
2020-06-15 21:08:10 +0300

    fix: load page

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,9 @@ import addNavigationClickHandler from './navigation/index';
 import addAuthorizationClickHandler from './authorization/index';
 import { isNewUser } from './utils/checks';
 
-
 window.onload = () => {
+  window.stop();
   if (isNewUser()) {
-    window.location.href = 'authorization.html';
     addAuthorizationClickHandler();
   }
   addNavigationClickHandler();
