c07a2bc7b154040f8aa2b60743191d71e1240e99
Marrlika <M.a.r.i.a_89@mail.ru>
2020-06-19 09:49:03 +0300

    feat: add status bar

--- src/js/speak-it/index.js
+++ src/js/speak-it/index.js
@@ -1,7 +1,10 @@
 import StartScreen from './StartScreen';
+import StatusBar from './StatusBar';
 
 const mainWrapper = document.querySelector('.speak-it_main');
-// const gameContainer = document.querySelector('.game-container');
+const gameContainer = document.querySelector('.game-container');
 
 const startScreen = new StartScreen();
 mainWrapper.append(startScreen.generateStartScreen());
+const statusBar = new StatusBar();
+gameContainer.append(statusBar.generateStatusBar());
