89c927f515c0d62649412c590e23064cb6f68fc6
hanna_zaneuskaya <hanna_zaneuskaya@epam.com>
2020-07-01 20:47:34 +0300

    refactor: remove test methods

--- src/js/statistics/index.js
+++ src/js/statistics/index.js
@@ -3,23 +3,6 @@ import {
   Statistics
 } from './components/statistics';
 
-let stat = null;
-
 export default async function initStatistics() {
-  stat = new Statistics('daily-2');
-
-  // await stat.updateStatistics('black', true, 3);
-  //  await stat.updateStatistics('red', true, 1);
-  //  await stat.updateStatistics('bag', true, 4);
-  //  await stat.updateStatistics('lang', false, 4);
-  //  await stat.updateStatistics('dog', true, 2);
-  //  await stat.updateStatistics('cat', false, 2);
-
-  document.querySelector('.test').addEventListener('click', () => {
-    stat.showGlobalStatistics();
-  });
-
-  document.querySelector('.test2').addEventListener('click', () => {
-    stat.showTemporaryStatistics();
-  });
+  const stat = new Statistics('daily-2');
 };
