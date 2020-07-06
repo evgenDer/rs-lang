36ebacbd279d6a09fe7a759a1ba27acb01936fc1
Marrlika <M.a.r.i.a_89@mail.ru>
2020-07-06 09:15:24 +0300

    feat: add word category switching

--- src/js/vocabulary/index.js
+++ src/js/vocabulary/index.js
@@ -1,5 +1,7 @@
 import { getSettings } from '../api/settings';
 import { getAggregatedUserWords } from '../api/userWords';
+import { createElementObj } from '../utils/create';
+import { calculateRepeatTiming } from '../words/updateWordState';
 import Card from './Card';
 
 const FILTERS = {
@@ -8,28 +10,37 @@ const FILTERS = {
   learning: {'$and':[{'userWord.difficulty':{'$in':['normal', 'easy']}, 'userWord.optional.mode': {'$not': {'$eq':'deleted'}}}]},
 }
 
+const wordsWrapper = document.querySelector('.vocabulary_cards-wrapper');
+const audioObj = new Audio();
+const cards = [];
+let configuration = '';
 
-function generateDataForCards(configuration, UserWordsData) {
+function generateDataForCards(UserWordsData, wordCategory) {
   const dataForCards = UserWordsData.map((wordData) => {
     const data = {
       lastUpdateDate: wordData.userWord.optional.lastUpdateDate,
       repeatCount: wordData.userWord.optional.repeatCount,
+
       audio: wordData.audio,
       word: wordData.word,
       wordTranslate: wordData.wordTranslate,
     }
-    if(configuration.showImageAssociation) {
+    if(wordCategory !== 'deleted') {
+      data.repeatTiming = calculateRepeatTiming(wordData.userWord);
+    }
+
+    if(configuration.optional.showImageAssociation) {
       data.image = wordData.image;
     }
-    if(configuration.showWordTranscription) {
+    if(configuration.optional.showWordTranscription) {
       data.transcription = wordData.transcription;
     }
-    if(configuration.showSentenceExplanation) {
+    if(configuration.optional.showSentenceExplanation) {
       data.textMeaning = wordData.textMeaning;
       data.textMeaningTranslate = wordData.textMeaningTranslate;
       data.audioMeaning = wordData.audioMeaning;
     }
-    if(configuration.showExplanationExample) {
+    if(configuration.optional.showExplanationExample) {
       data.textExample = wordData.textExample;
       data.textExampleTranslate = wordData.textExampleTranslate;
       data.audioExample = wordData.audioExample;
@@ -39,23 +50,47 @@ function generateDataForCards(configuration, UserWordsData) {
   return dataForCards;
 }
 
-export  default async function initVocabularyPage() {
-  const wordsWrapper = document.querySelector('.vocabulary_cards-wrapper');
-  const audioObj = new Audio();
-  const configuration = await getSettings();
-// getAllUserWords().then((result) => console.log(result));
-// getAggregatedUserWords({'userWord.difficulty':{'$eq':'normal'}}).then((result) => console.log(result));
-  const UserWordsData = await getAggregatedUserWords(FILTERS.learning, 3600);
-  const dataForCards = generateDataForCards(configuration.optional, UserWordsData[0].paginatedResults);
-  const cards = [];
-  dataForCards.forEach((cardData) => {
-    const card = new Card(cardData);
-    cards.push(card);
-    wordsWrapper.append(card.generate((audioSrc) => {
-      audioObj.pause();
-      audioObj.src = audioSrc;
-      audioObj.play();
-    }));
-  });
+function playAudio(audioSrc) {
+  audioObj.pause();
+  audioObj.src = audioSrc;
+  audioObj.play();
+}
 
+function ErrorMassage() {
+  const errorMessage = createElementObj({ tagName: 'p', classNames: `vocabulary__error-message`, textContent: 'Слова данной категории отсутствуют' });
+  wordsWrapper.innerHTML = '';
+  wordsWrapper.append(errorMessage);
+}
+
+async function drawCards(wordCategory) {
+  const UserWordsData = await getAggregatedUserWords(FILTERS[wordCategory], 3600);
+  if(UserWordsData[0].paginatedResults.length === 0) {
+    ErrorMassage();
+  } else {
+    const dataForCards = generateDataForCards(UserWordsData[0].paginatedResults, wordCategory);
+    wordsWrapper.innerHTML = '';
+    cards.length = 0;
+    let displayRestoreButton = false;
+    if(wordCategory !== 'learning') {
+      displayRestoreButton = true;
+    }
+    dataForCards.forEach((cardData) => {
+      const card = new Card(cardData);
+      cards.push(card);
+      wordsWrapper.append(card.generate((audioSrc) => playAudio(audioSrc), displayRestoreButton));
+    });
+  }
+
+}
+
+export  default async function initVocabularyPage() {
+  configuration = await getSettings();
+  drawCards('learning');
+
+  const switcher = document.querySelector('.vocabulary__switcher');
+  switcher.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    drawCards(event.target.id);
+  }
+});
 }
