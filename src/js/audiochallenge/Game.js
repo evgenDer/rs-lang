be5a56915b6fcf91dac69e0025b0b85d7d1b6c48
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-07-06 12:41:25 +0300

    fix: audio playing

--- src/js/audiochallenge/Game.js
+++ src/js/audiochallenge/Game.js
@@ -13,6 +13,7 @@ import { increaseWordErrorCount, increaseWordReferenceCount } from '../words/upd
 import { getAllUserWords, updateUserWord } from '../api/userWords';
 import { AUDIO_B64, IMG_B64, WORD_STATE } from '../utils/constants';
 import { saveCustomConfiguration } from '../configuration/index';
+import playAudio from '../helpers/audio';
 
 
 const loader = document.querySelector('.audiochallenge__load-page');
@@ -415,10 +416,6 @@ export class Game {
   }
 
   addAudioButtonsClickHandler() {
-    const playAudio = (src) => {
-      const audio = new Audio(src);
-      audio.play();
-    }
     const playWordAudio = () => playAudio(this.task.wordSpeakerSrc);
     const playExampleAudio = () => playAudio(this.task.exampleSpeakerSrc);
 
