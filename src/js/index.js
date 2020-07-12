3095e51c4edea09f7092dc94bcb70760db2822ce
AnnieBunnyGH <kostya4d@gmail.com>
2020-07-12 20:02:24 +0300

    fix: index inmport fix

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import { initConfigurationPage } from './configuration/index';
 import initStatistics from './statistics/index';
 import { updPageContent } from './main-page/index';
 import initVocabularyPage from './vocabulary/index';
+import addBtnStartEventListener from './determinationLevel/index';
 //импорт модуля Изучения слов. Позволяет использовать хтмл элемент карточки <card-word></card-word>
 import './learningWords/learningWordsPage';
 import { getTokenTime } from './helpers/tokenHeleper';
