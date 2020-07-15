a2247c3a3546b152a4f5d914094579c9fae245e7
AnnieBunnyGH <kostya4d@gmail.com>
2020-07-15 15:06:40 +0300

    fix: backgroundResult fix

--- src/js/learningWords/learningScreen/functions/saveSettings.js
+++ src/js/learningWords/learningScreen/functions/saveSettings.js
@@ -1,4 +1,4 @@
-import { getConfiguration, saveCustomConfiguration } from '../../../configuration/index';
+import { getConfiguration } from '../../../configuration/index';
 import * as configurationService from '../../../api/settings';
 
 export default async function saveSettingsFromLearningWords(
@@ -6,16 +6,16 @@ export default async function saveSettingsFromLearningWords(
   hardMode = learningScreenElemen.settings.learning.isHardMode,
 ) {
   const prevConfiguration = await getConfiguration();
-
+  console.log(prevConfiguration);
   prevConfiguration.showWordTranslation = learningScreenElemen.settings.showWordTranslation;
   prevConfiguration.enableAutomaticAudio = learningScreenElemen.settings.enableAutomaticAudio;
   prevConfiguration.showNewWordTranslation = learningScreenElemen.settings.showNewWordTranslation;
   prevConfiguration.showSentenceTranslation = learningScreenElemen.settings.showSentenceTranslation;
-  prevConfiguration.learning = saveCustomConfiguration('learning', {
+  prevConfiguration.learning = {
     isHardMode: hardMode,
     groupNumber: learningScreenElemen.settings.learning.groupNumber,
     learningWordsPage: learningScreenElemen.settings.learning.learningWordsPage,
-  });
-
+  };
+  console.log(prevConfiguration);
   await configurationService.upserSettings({ optional: prevConfiguration });
 }
